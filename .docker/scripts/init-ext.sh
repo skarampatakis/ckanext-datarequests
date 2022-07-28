3c8145270d0d46714f6708b1bfc6f2751023e500
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-07-29 09:11:56 +1000

    [QOL-9055] simplify test requirement installation
    - use a single function that automatically handles suffixes and alternatives

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -5,31 +5,31 @@
 set -e
 
 install_requirements () {
-    PROJECT_DIR="$1"
-    for filename in requirements-$PYTHON_VERSION.txt requirements.txt pip-requirements.txt; do
-        if [ -f "$PROJECT_DIR/$filename" ]; then
+    PROJECT_DIR=$1
+    shift
+    # Identify the best match requirements file, ignore the others.
+    # If there is one specific to our Python version, use that.
+    for filename_pattern in $*; do
+        if [ -f "$PROJECT_DIR/${filename_pattern}-$PYTHON_VERSION.txt" ]; then
+            pip install -r "$PROJECT_DIR/$filename"
+            return 0
+        fi
+    done
+    for filename_pattern in $*; do
+        if [ -f "$PROJECT_DIR/${filename_pattern}.txt" ]; then
             pip install -r "$PROJECT_DIR/$filename"
             return 0
         fi
     done
 }
 
-install_dev_requirements () {
-    PROJECT_DIR="$1"
-    for filename in dev-requirements-$PYTHON_VERSION.txt requirements-dev-$PYTHON_VERSION.txt requirements-dev.txt dev-requirements.txt; do
-        if [ -f "$PROJECT_DIR/$filename" ]; then
-            pip install -r "$PROJECT_DIR/$filename"
-            return 0
-        fi
-    done
-}
 
 if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
-install_dev_requirements .
+install_requirements . dev-requirements requirements-dev
 for extension in . `ls -d $VENV_DIR/src/ckanext-*`; do
-    install_requirements $extension
+    install_requirements $extension requirements pip-requirements
 done
 python setup.py develop
 installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1|")
