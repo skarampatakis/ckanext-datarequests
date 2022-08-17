2b8eaec8fb00629ea2f02244a47975d6f8063731
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-08-18 09:17:47 +1000

    [QOL-9252] oops set Python executable in test Docker container

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -14,10 +14,12 @@ sed -i -e "s/##//" docker-compose.yml
 # Pull the latest images.
 ahoy pull
 
+PYTHON=python
 if [ "$CKAN_VERSION" = "2.8" ] || [ "$CKAN_VERSION" = "2.9-py2" ]; then
     PYTHON_VERSION=py2
 else
     PYTHON_VERSION=py3
+    PYTHON="${PYTHON}3"
 fi
 
 sed "s|{CKAN_VERSION}|$CKAN_VERSION|g" .docker/Dockerfile-template.ckan \
